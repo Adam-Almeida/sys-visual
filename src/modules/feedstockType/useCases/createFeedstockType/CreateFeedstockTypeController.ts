d4ab1d8215cae53366a0984675fda26969c7884c
adam-almeida <adamportifolio@gmail.com>
2022-10-10 20:11:13 -0300

    feat: create feedstocktype module

--- src/modules/feedstockType/useCases/createFeedstockType/CreateFeedstockTypeController.ts
+++ src/modules/feedstockType/useCases/createFeedstockType/CreateFeedstockTypeController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import { slugIFy } from '@/utils/slugIFy'
+import { BadRequestError } from '@/errors/ApiErrors'
+import { CreateFeedstockTypeUseCase } from './CreateFeedstockTypeUseCase'
 
 const slugFy = new slugIFy()
 
@@ -7,8 +9,19 @@ export class CreateFeedstockTypeController {
   async handle(req: Request, res: Response) {
     const { name } = req.body
 
+    if (!name) {
+      throw new BadRequestError(
+        'O nome para o tipo de insumo não pode estar em branco.'
+      )
+    }
+
     const slug = slugFy.slug(name)
 
-    res.json({ name, slug })
+    const createFeedstockUseCase = new CreateFeedstockTypeUseCase()
+    const result = await createFeedstockUseCase.execute({ name, slug })
+
+    return res
+      .status(200)
+      .json({ message: 'Tipo de insumo cadastrado com sucesso', data: result })
   }
 }
--- src/modules/feedstockType/useCases/createFeedstockType/CreateFeedstockTypeUseCase.ts
+++ src/modules/feedstockType/useCases/createFeedstockType/CreateFeedstockTypeUseCase.ts
@@ -1,11 +1,35 @@
+import { prisma } from '@/database/prismaClient'
+import { BadRequestError } from '@/errors/ApiErrors'
 
-interface IRequestFeedstockType{
-    name: string
-    slug: string
+interface IRequestFeedstockType {
+  name: string
+  slug: string
 }
 
-export class CreateFeedstockTypeUseCase{
-    async export({name, slug}: IRequestFeedstockType){
+export class CreateFeedstockTypeUseCase {
+  async execute({ name, slug }: IRequestFeedstockType) {
+    const feedstockType = await prisma.feedstockType.findFirst({
+      where: {
+        slug: {
+          equals: slug,
+        },
+      },
+      select: {
+        id: true,
+      },
+    })
 
+    if (feedstockType) {
+      throw new BadRequestError('Este tipo de insumo já está cadastrado.')
     }
+
+    const result = await prisma.feedstockType.create({
+      data: {
+        name,
+        slug,
+      },
+    })
+
+    return result
+  }
 }
